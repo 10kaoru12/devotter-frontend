2f8d97cf4d7bbccb87e7c52a1c814c26b08ed36d
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2019-12-26 22:32:50 +0900

    内容の修正

---
+++ src/components/guestUserScreen.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/thai monk.svg')"
+          class="my-3"
+          contain
+          height="200"
+        ></v-img>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
