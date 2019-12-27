010efc3c64d127f273ddc242c6a6d78b9706e147
Kaoru Ishizawa <kaoru.ishizawa@nnlife.co.jp>
2019-12-27 19:49:59 +0900

    axiosのインストール

--- src/components/guestUserScreen.vue
+++ src/components/guestUserScreen.vue
@@ -1,36 +1,70 @@
 <template>
-  <v-container fluid fill-height>
+  <v-container pa-12 fill-height>
     <v-layout align-center text-center wrap>
       <v-flex xs12>
         <v-img :src="require('../assets/thai monk.svg')" class="my-3" contain height="200"></v-img>
-        <h1>
-          「Devotter」
-          <br />その精進
+        <h1>「Devotter」</h1>
+        <h2>
+          その精進
           <br />ツイートしませんか
-        </h1>
+        </h2>
         <br />
-        <h5>
-          このアプリケーションは、1日1回AtCoderでのAC数を
-          <br />Twitterにツイートしてくれるアプリケーションです。
-        </h5>
+        <h5>このアプリケーションは、1日1回AtCoderでのAC数をTwitterにツイートしてくれるアプリケーションです。</h5>
         <br />
-        <v-btn @click="signin" color="#00acee">Signin Twitter</v-btn>
+        <v-form>
+          <v-text-field
+            @change="changeField"
+            type="text"
+            v-model="atcoderId"
+            label="AtCoderID"
+            required
+          ></v-text-field>
+        </v-form>
+        <v-btn @click="signin" v-bind:disabled="isnull" color="#00acee" rounded>Signin To Twitter</v-btn>
+        {{userInfo}}
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-
-import firebase from 'firebase';
+import firebase from "firebase";
+import axios from "axios";
 
 export default {
   name: "guestUserScreen",
   methods: {
     signin: function() {
       const provider = new firebase.auth.TwitterAuthProvider();
-      firebase.auth().signInWithPopup(provider);
+      firebase.auth().signInWithRedirect(provider);
+      // .then(function(result) {
+      //   var token = result.credential.accessToken;
+      //   var secret = result.credential.secret;
+      // });
+    },
+    changeField: function() {
+      !this.atcoderId ? (this.isnull = true) : (this.isnull = false);
+
     }
+  },
+  created: function() {
+    axios
+      .get("https://kenkoooo.com/atcoder/resources/ac.json")
+      .then(response => {
+        this.userInfo = response;
+      })
+      .catch(() => {
+        this.userInfo = "無効なAtCoderID名です。";
+      });
+    // eslint-disable-next-line no-console
+    console.log(this.userInfo);
+  },
+  data: function() {
+    return {
+      atcoderId: "",
+      isnull: true,
+      userInfo: ""
+    };
   }
 };
 </script>
